c041ab91efc1ac7f3fb8a220e84c82aa733ba470
hrl60 <hrl60@yahoo.com>
2020-04-02 01:29:26 +0430

    Updates

---
+++ precache-manifest.12afddd49cf0e778628c6cd4abf6a50d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989f7f4e9bf4f27de84c4abce2af6894",
+    "url": "/todo-test-react/index.html"
+  },
+  {
+    "revision": "23486a53860d8c3a3ecc",
+    "url": "/todo-test-react/static/css/main.7e3a81b3.chunk.css"
+  },
+  {
+    "revision": "ff920c4c0fd08331cbcb",
+    "url": "/todo-test-react/static/js/2.853db0a2.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todo-test-react/static/js/2.853db0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23486a53860d8c3a3ecc",
+    "url": "/todo-test-react/static/js/main.8179a114.chunk.js"
+  },
+  {
+    "revision": "3e94846e50b9ab2e4f27",
+    "url": "/todo-test-react/static/js/runtime-main.b0a1359c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
